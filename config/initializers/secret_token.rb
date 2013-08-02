49bdf5a20aa4b51980f4434cff7b7945082a1dad
lucasjmoore <lucasmoore88@gmail.com>
2013-08-02 17:15:04 -0400

    Clean built rails file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Booklist::Application.config.secret_key_base = 'a402d7cf9dcaa0060976b59d0c3484b0c138bcab8c3c04b435239e309e44691cfacaa44511a4268749e19f59adf00a6df396badaf56b62c5458acc8be340724f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Booklist::Application.config.session_store :cookie_store, key: '_booklist_session'
