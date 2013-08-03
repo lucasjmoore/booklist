e2ebdfc113aa02f4c3407e3a9042f4863b953645
lucasjmoore <lucasmoore88@gmail.com>
2013-08-03 14:11:59 -0400

    Set up basic Book model and books controllers, and a simple README form.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Booklist::Application.routes.draw do
+  get "books/index"
+  get "books/show"
+  get "books/edit"
+  get "books/update"
+  get "books/new"
+  get "books/create"
+  get "books/destroy"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  resources :books
 
+  root 'book#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
